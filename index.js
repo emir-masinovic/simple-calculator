365761d3c7346f0f41d418fc2404f31bd93df2a0
emir-masinovic <emir3710549@gmail.com>
2023-05-17 02:08:46 -0400

    Calculator for desktop finished

--- index.js
+++ index.js
@@ -27,41 +27,88 @@ const display = document.querySelector(".display");
 let inputArray = "";
 
 buttons.forEach(({ text, className }) => {
-  const buttonEl = document.createElement("button");
-  buttonEl.textContent = text;
-  buttonEl.className = className;
-  buttonEl.addEventListener("click", (event) => {
-    event.preventDefault();
-    let pressedButton = event.target.innerText;
-    switch (pressedButton) {
-      case "AC":
-        inputArray = "";
-        display.textContent = "0";
-        console.log(
-          `Pressed button: ${pressedButton}. Input array is: ${inputArray}`
-        );
-        break;
-      case "=":
-        if (inputArray === "") { return; } //prettier-ignore
-        // inputArray = calculate(inputArray);
-        inputArray = evaluateExpression(inputArray);
-        display.textContent = inputArray;
-        console.log(
-          `Pressed button: ${pressedButton}. Input array is: ${inputArray}`
-        );
-        break;
-      default:
-        inputArray += pressedButton;
-        console.log(
-          `Pressed button: ${pressedButton}. Input array is: ${inputArray}`
-        );
-        display.textContent = inputArray;
-        break;
-    }
-  });
-
-  buttonsContainer.appendChild(buttonEl);
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = text;
+  buttonElement.className = className;
+  buttonElement.addEventListener("click", handleButtonClick);
+  buttonsContainer.appendChild(buttonElement);
 });
+
+function handleButtonClick(event) {
+  event.preventDefault();
+
+  const pressedButton = event.target.innerText;
+  switch (pressedButton) {
+    case "AC":
+      handleACButton();
+      break;
+    case "=":
+      handleEqualsButton();
+      break;
+    case "x²":
+      handleSquareButton();
+      break;
+    case "√":
+      handleRootButton();
+      break;
+    case "%":
+      handlePercentageButton();
+      break;
+    default:
+      handleNumberButton(pressedButton);
+      break;
+  }
+}
+
+function handleNumberButton(pressedButton) {
+  inputArray += pressedButton;
+  updateDisplay();
+}
+
+function handlePercentageButton() {
+  inputArray += "/100*";
+  updateDisplay();
+}
+
+function handleACButton() {
+  display.textContent = "0";
+  inputArray = "";
+}
+
+function handleSquareButton() {
+  inputArray += "**2";
+  handleEqualsButton();
+}
+
+function handleRootButton() {
+  const regex = /-/g;
+  const matches = inputArray.match(regex);
+
+  if (matches && matches.includes("-")) {
+    inputArray = "ERROR";
+  } else {
+    inputArray += "**(1/2)";
+  }
+
+  updateDisplay();
+  handleEqualsButton();
+}
+
+function handleEqualsButton() {
+  if (inputArray === "") return;
+  const result = evaluateExpression(inputArray);
+  inputArray = result.toString(); // Assign the evaluated result back to inputArray
+  updateDisplay();
+}
+
+function updateDisplay() {
+  if (inputArray.includes("/100*")) {
+    display.textContent = inputArray.replace("/100*", "%");
+  } else {
+    display.textContent = inputArray;
+  }
+}
+
 function evaluateExpression(str) {
   return Function(`'use strict'; return (${str})`)();
 }
